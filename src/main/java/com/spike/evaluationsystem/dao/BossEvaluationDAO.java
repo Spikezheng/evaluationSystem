285aba3f8675f06e0705c957e8d2b56dc5e4cab8
BushiJhon <1172905107@qq.com>
2019-11-15 18:02:02 +0800

    the BossEvaluation DAO

--- src/main/java/com/spike/evaluationsystem/dao/BossEvaluationDAO.java
+++ src/main/java/com/spike/evaluationsystem/dao/BossEvaluationDAO.java
@@ -48,6 +48,55 @@ public class BossEvaluationDAO {
         return bossEvaluation;
     }
 
+    //插入
+    public int add(BossEvaluation bossEvaluation){
+        int bossEvaluationId = 0;
+        String sql = "insert into bossEvaluation values( null , ?, ?, ?, ?, ?, ?, ?)";
+
+        try(Connection connection = this.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setFloat(1, bossEvaluation.getStaff_corporate_identity());
+            preparedStatement.setFloat(2, bossEvaluation.getStaff_comunication_ability());
+            preparedStatement.setFloat(3, bossEvaluation.getStaff_responsibility());
+            preparedStatement.setFloat(4, bossEvaluation.getStaff_enthusiasm());
+            preparedStatement.setFloat(5, bossEvaluation.getStaff_task_completion_time());
+            preparedStatement.setFloat(6, bossEvaluation.getStaff_feedback_time());
+            preparedStatement.setFloat(7, bossEvaluation.getStaff_task_response_tim());
+            preparedStatement.execute();
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()){
+                bossEvaluationId = resultSet.getInt(1);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return bossEvaluationId;
+    }
+
+    //更新
+    public void update(int bossEvaluationId, BossEvaluation bossEvaluation){
+        String sql = "update bossEvaluation set staff_corporate_identity = ?, staff_comunication_ability = ?, staff_responsibility = ?, " +
+                "staff_enthusiasm = ?, staff_task_completion_time = ?, staff_feedback_time = ?, staff_task_response_tim = ? where bossEvaluationId = ?";
+
+        try(Connection connection = this.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setFloat(1, bossEvaluation.getStaff_corporate_identity());
+            preparedStatement.setFloat(2, bossEvaluation.getStaff_comunication_ability());
+            preparedStatement.setFloat(3, bossEvaluation.getStaff_responsibility());
+            preparedStatement.setFloat(4, bossEvaluation.getStaff_enthusiasm());
+            preparedStatement.setFloat(5, bossEvaluation.getStaff_task_completion_time());
+            preparedStatement.setFloat(6, bossEvaluation.getStaff_feedback_time());
+            preparedStatement.setFloat(7, bossEvaluation.getStaff_task_response_tim());
+            preparedStatement.setInt(8, bossEvaluationId);
+            preparedStatement.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return;
+    }
+
     @Override
     public String toString(){
         return super.toString();
